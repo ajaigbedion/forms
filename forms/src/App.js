35d14fa399635a82bf7dd6aac218fe0130b7939e
Allende Aigbedion <ajaigbedion@gmail.com>
2020-06-29 14:30:34 -0400

    First commit

---
+++ forms/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Forms extends Component{
+  render(){
+    return(
+      <form>
+        <label htmlFor="name">
+          Name: 
+        </label>
+      </form>
+    )
+  };
+};
+
+export default Forms;
